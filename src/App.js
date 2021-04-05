353cfc156678372614bc08df0293cbdeeb6d8f90
عبد المولى أحمد <ahmedabdelmoula1@gmail.com>
2021-04-05 10:02:19 +0100

    1st

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Appnav from "./Components/Appnav";
+import UserList from "./Components/UserList";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import UserProfile from "./Components/UserProfile";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Appnav />
+        <Switch>
+          <Route exact path="/">
+            <UserList />
+          </Route>
+          <Route path="/profile/:userId" component={UserProfile}></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
